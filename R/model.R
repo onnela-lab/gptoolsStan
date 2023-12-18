10309b34a2f1203d2717503e9ba34b6f01b90d17
Till Hoffmann <tillahoffmann@gmail.com>
2023-12-18 19:49:57 +0100

    Update package in response to comments from CRAN review.

--- R/model.R
+++ R/model.R
@@ -1,6 +1,18 @@
-#' Get the gptools include path for compiling Stan programs with `cmdstanr`.
+#' Get the 'gptoolsStan' include path for compiling 'Stan' programs.
 #'
 #' @export
+#' @return Path to the directory containing source files for 'gptoolsStan' as characters.
+#' @examples
+#' \dontrun{
+#' library(cmdstanr)
+#' library(gptoolsStan)
+#'
+#' # Compile the model with paths set up to include 'Stan' sources from 'gptoolsStan'.
+#' model <- cmdstan_model(
+#'   stan_file="/path/to/your/model.stan",
+#'   include_paths=gptools_include_path(),
+#' )
+#' }
 gptools_include_path <- function() {
     system.file("extdata", package="gptoolsStan")
 }
